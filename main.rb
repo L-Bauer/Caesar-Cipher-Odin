57972c5e328e7785205bb2897dd4b4e12bd16cad
L_Bauer <85202822+L-Bauer@users.noreply.github.com>
2022-08-29 23:38:55 +0000

    Setting up pseudocode

--- main.rb
+++ main.rb
@@ -1,1 +1,17 @@
-puts "Hello, World!"
+# Take a string then shift letters by X int
+# Convert string to array then convert letters to numbers
+# Shift letter numbers by X int
+
+# def cipher(in_string, shift_number)
+# end
+
+def convert_to_num(string_convert)
+  num_array = []
+  string_array = string_convert.split('')
+  string_array.each do |i|
+    num_array[i] = string_array[i].ord
+  end
+  num_array
+end
+
+convert_to_num('Hello World!')
