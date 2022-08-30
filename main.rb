a5dbdaa5bc41bb26645699f42604a4dbf8bc5201
Lucas Bauer <lucassoccer95@gmail.com>
2022-08-29 19:52:43 -0500

    Setup functions
    Created two functions. One function checks if the numbers is a letter
    based on ASCII. The other function is going to shift the numbers of the
    letters.

--- main.rb
+++ main.rb
@@ -5,13 +5,29 @@
 # def cipher(in_string, shift_number)
 # end
 
-def convert_to_num(string_convert)
+def convert_string_to_num(string_convert)
     num_array = []
     string_array = string_convert.split('')
     string_array.each do |letter|
-      num_array << letter.ord
+        num_array << letter.ord
     end
-    puts num_array
+    return num_array
 end
 
-convert_to_num('hello world!')
+def shift_numbers(shift_array, shift_by_number)
+    shift_array.each do |number|
+        numbers_is_leter(number)
+    end
+end
+
+def numbers_is_leter(letter_num) 
+    if letter_num < 122 and letter_num > 97
+        puts "Small letter"
+    elsif letter_num < 90 and letter_num > 65
+        puts "Large letter"
+    else 
+        puts "Not a letter"
+    end
+end
+
+shift_numbers(convert_string_to_num('Hello World!'), 5)
